2e377fefd3e7a8ace61a7344f1b95ab7c2e78c53
Juuso Paakkunainen <juusop2k12@gmail.com>
2025-12-17 19:56:09 +0200

    2 part 1

--- 2025/2/src/main.rs
+++ 2025/2/src/main.rs
@@ -1,15 +1,41 @@
-// This is a comment, and is ignored by the compiler.
-// You can test this code by clicking the "Run" button over there ->
-// or if you prefer to use your keyboard, you can use the "Ctrl + Enter"
-// shortcut.
-
-// This code is editable, feel free to hack it!
-// You can always return to the original code by clicking the "Reset" button ->
-
-// This is the main function.
 fn main() {
-    // Statements here are executed when the compiled binary is called.
+    use std::io::stdin;
+    let mut s = String::new();
+    println!("Enter puzzle input");
+    stdin().read_line(&mut s).expect("Incorrect string");
+    if let Some('\n') = s.chars().next_back() {
+        s.pop();
+    }
+    if let Some('\r') = s.chars().next_back() {
+        s.pop();
+    }
 
-    // Print text to the console.
-    println!("Hello World!");
+    let split = s.split(',');
+    let mut total = 0;
+
+    for x in split {
+        let mut bounds = x.split('-');
+        let lower_b: i64 = bounds.next().unwrap().parse().unwrap();
+        let upper_b: i64 = bounds.next().unwrap().parse().unwrap();
+        // println!("Bounds = {} - {}", lower_b, upper_b);
+
+        for n in lower_b..upper_b + 1 {
+            // println!("n = {}", n);
+            let s = n.to_string();
+            let len = s.len();
+            if len % 2 != 0 {
+                continue;
+            }
+            let middle = len / 2;
+            let left: String = s[..middle].parse().ok().unwrap();
+            let right: String = s[middle..].parse().ok().unwrap();
+
+            // println!("Left = {}, Right {}", left, right);
+
+            if left == right {
+                total = total + n;
+            }
+        }
+    }
+    println!("Total is {}", total);
 }
